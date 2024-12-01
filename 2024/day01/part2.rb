cf2fb219df2678e66663b6e714488ac26f8b7da7
Kevin Marsh <kevin.marsh@gmail.com>
2024-12-01 10:53:37 -0500

    Solved 2024 Day 1, Part 2

---
+++ 2024/day01/part2.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Calculate a total similarity score by adding up each number in the left list
+# after multiplying it by the number of times that number appears in the right list.
+
+sum = 0
+left = []
+right = []
+
+ARGF.read
+    .split("\n")
+    .each {|l| n = l.split.map(&:to_i); left << n[0]; right << n[1] }
+
+left.each_with_index do |l, i|
+  times = right.count(l)
+
+  sum += l * times
+end
+
+puts sum
